9aef8ed26e22064a49f59f6b340ad7d591d0fd69
bls <blair.strang@auspost.com.au>
2019-02-21 17:09:00 +1100

    Refactor to collapse layers. Add first cut of metrics.

--- inkfish.go
+++ inkfish.go
@@ -1,54 +1,109 @@
+// CREDITS:
+// * https://github.com/kr/mitm/blob/master/mitm.go
+//     Copyright (c) 2015 Keith Rarick
+//     MIT License
+// * https://github.com/elazarl/goproxy
+//     Copyright (c) 2012 Elazar Leibovich
+//     BSD 3-Clause "New" or "Revised" License
+
 package inkfish
 
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"github.com/pkg/errors"
+	"github.com/rcrowley/go-metrics"
+	"io"
 	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
+	"net/http/httputil"
 	"strconv"
 	"strings"
-	//"strconv"
-	//"strings"
+	"time"
 )
 
-const AccessDenied = `
-************************
-* PROXY DENIED REQUEST *
-************************
-`
+type Metrics struct {
+	Registry         metrics.Registry
+	AcceptedRequests metrics.Counter
+	DeniedRequests   metrics.Counter
+	MitmConnects     metrics.Counter
+	BypassConnects   metrics.Counter
+	DeniedConnects   metrics.Counter
+}
 
 type Inkfish struct {
-	Acls             []Acl
-	Passwd           []UserEntry
+	Acls   []Acl
+	Passwd []UserEntry
+
+	// Maintains an ip -> tag map, for access control based on instance metadata
 	MetadataProvider MetadataProvider
-	ConnectFilter    func(host string, port int) bool
-	Proxy            *Proxy
-	CertSigner       *CertSigner
+
+	// Decides whether to allow a CONNECT call by examining the host and port only
+	ConnectPolicy func(host string, port int) bool
+
+	// Generates leaf certs for TLS connections.
+	CertSigner *CertSigner
+
+	// TLSServerConfig specifies the tls.Config to use when generating leaf cert using CA.
+	TLSServerConfig *tls.Config
+
+	// TLSClientConfig specifies the tls.Config to use when establishing an upstream
+	// connection for proxying.
+	TLSClientConfig *tls.Config
+
+	// FlushInterval specifies the flush interval to flush to the client while copying
+	// the response body. If zero, no periodic flushing is done.
+	FlushInterval time.Duration
+
+	// Metrics! Metrics!
+	Metrics Metrics
 }
 
 func NewInkfish(signer *CertSigner) *Inkfish {
 	proxy := &Inkfish{
-		Proxy: &Proxy{
-			TLSServerConfig: &tls.Config{
-				MinVersion: tls.VersionTLS12,
-			},
-			CertSigner: signer,
+		TLSServerConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
 		},
+		CertSigner: signer,
 	}
-	proxy.Proxy.Wrap = func(connectReq *http.Request, scheme string, upstream http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if allowed := proxy.RequestFilter(connectReq, scheme, w, r); allowed {
-				upstream.ServeHTTP(w, r)
-			} else {
-				// We dropped it...
-			}
-		})
-	}
-	proxy.Proxy.ConnectFilter = func(w http.ResponseWriter, r *http.Request) ConnectAction {
-		return proxy.FilterConnect(w, r)
-	}
+	proxy.Metrics.Init()
 	return proxy
+}
+
+func (m *Metrics) Init() {
+	m.Registry = metrics.NewRegistry()
+
+	metrics.RegisterDebugGCStats(m.Registry)
+	metrics.RegisterRuntimeMemStats(m.Registry)
+
+	m.MitmConnects = metrics.NewCounter()
+	m.BypassConnects = metrics.NewCounter()
+	m.DeniedConnects = metrics.NewCounter()
+	m.AcceptedRequests = metrics.NewCounter()
+	m.DeniedRequests = metrics.NewCounter()
+
+	_ = metrics.Register("connect.mitm.count", m.MitmConnects)
+	_ = metrics.Register("connect.bypass.count", m.BypassConnects)
+	_ = metrics.Register("connect.denied.count", m.DeniedConnects)
+	_ = metrics.Register("request.accepted.count", m.AcceptedRequests)
+	_ = metrics.Register("request.denied.count", m.DeniedRequests)
+}
+
+func (m *Metrics) StartCapture() {
+	go metrics.CaptureDebugGCStats(m.Registry, time.Second*5)
+	go metrics.CaptureRuntimeMemStats(m.Registry, time.Second*5)
+}
+
+func (proxy *Inkfish) requestHandler(connectReq *http.Request, scheme string, upstream http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if allowed := proxy.filterRequest(connectReq, scheme, w, r); allowed {
+			upstream.ServeHTTP(w, r)
+		}
+		// If the request was dropped, proxy.filterRequest sent a response.
+	})
 }
 
 // Set the CA certificate presented by the proxy from PEM files
@@ -73,63 +128,81 @@ func (proxy *Inkfish) SetCA(caCert, caKey []byte) error {
 	if ca.Leaf, err = x509.ParseCertificate(ca.Certificate[0]); err != nil {
 		return errors.Wrap(err, "failed to parse CA certificate")
 	}
-	proxy.Proxy.CertSigner = NewCertSigner(&ca)
+	proxy.CertSigner = NewCertSigner(&ca)
 	return nil
 }
 
-func defaultConnectFilter(host string, port int) bool {
+func defaultConnectPolicy(host string, port int) bool {
+	// By default, block CONNECT to ports other than 443
 	return port == 443
 }
 
-func sendAccessDenied(w http.ResponseWriter) {
+func sendAccessDenied(w http.ResponseWriter, detail string) {
+	// This sends blocked site URLs etc back in errors for diagnostic purposes.
+	// We don't URL-escape because we don't actually expect responses to go back
+	// to a browser. Inkfish is for machines, not humans so these errors will go
+	// into log files. Doesn't hurt to set a few paranoia headers though.
+	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("Content-Security-Policy", "script-src: 'none';")
 	w.WriteHeader(403)
-	w.Write([]byte(AccessDenied))
+	_, err := w.Write([]byte("INKFISH PROXY DENIED REQUEST: " + detail))
+	if err != nil {
+		// Write to client failed.
+		// TODO: handle a blip
+	}
 }
 
-func (proxy *Inkfish) FilterConnect(w http.ResponseWriter, r *http.Request) (action ConnectAction) {
-	host := r.Host
+func (proxy *Inkfish) filterConnect(w http.ResponseWriter, req *http.Request) (action ConnectAction) {
+	host := req.Host
 
 	// Handle a CONNECT request
 	hostFields := strings.Split(host, ":")
 	if len(hostFields) != 2 {
 		proxy.logConnect(ConnectLogEntry{
-			RemoteAddr:    r.RemoteAddr,
+			RemoteAddr:    req.RemoteAddr,
 			User:          "UNKNOWN",
 			ConnectTarget: host,
 			Result:        "DENY",
-			Reason:        "bad connect request",
+			Reason:        "no port number",
 		})
-		sendAccessDenied(w)
+		proxy.Metrics.DeniedConnects.Inc(1)
+		errMsg := fmt.Sprintf("CONNECT: no port number in: '%s'", host)
+		sendAccessDenied(w, errMsg)
 		return ConnectDeny
 	}
 	connectHost := hostFields[0]
 	connectPort, err := strconv.Atoi(hostFields[1])
 	if err != nil {
 		proxy.logConnect(ConnectLogEntry{
-			RemoteAddr:    r.RemoteAddr,
+			RemoteAddr:    req.RemoteAddr,
 			User:          "UNKNOWN",
 			ConnectTarget: host,
 			Result:        "DENY",
 			Reason:        "bad port number",
 		})
-		sendAccessDenied(w)
+		proxy.Metrics.DeniedConnects.Inc(1)
+		errMsg := fmt.Sprintf("CONNECT: bad port number in: '%s'", host)
+		sendAccessDenied(w, errMsg)
 		return ConnectDeny
 	}
 	var allowed bool
-	if proxy.ConnectFilter != nil {
-		allowed = proxy.ConnectFilter(connectHost, connectPort)
+	if proxy.ConnectPolicy != nil {
+		allowed = proxy.ConnectPolicy(connectHost, connectPort)
 	} else {
-		allowed = defaultConnectFilter(connectHost, connectPort)
+		allowed = defaultConnectPolicy(connectHost, connectPort)
 	}
 	if !allowed {
 		proxy.logConnect(ConnectLogEntry{
-			RemoteAddr:    r.RemoteAddr,
+			RemoteAddr:    req.RemoteAddr,
 			User:          "UNKNOWN",
 			ConnectTarget: host,
 			Result:        "DENY",
-			Reason:        "denied by connect filter",
+			Reason:        "CONNECT denied by policy",
 		})
-		sendAccessDenied(w)
+		proxy.Metrics.DeniedConnects.Inc(1)
+		errMsg := fmt.Sprintf("CONNECT denied by policy: '%s'", host)
+		sendAccessDenied(w, errMsg)
 		return ConnectDeny
 	}
 
@@ -138,10 +211,10 @@ func (proxy *Inkfish) FilterConnect(w http.ResponseWriter, r *http.Request) (act
 	// we need to read that out now as it will not be sent on "tunneled" HTTP requests.
 
 	var user string
-	user, err = proxy.authenticateClient(r)
-	if err != nil || user == badUser {
-		proxy.ctxLogf("client: %v: authentication error during connect: %v", r.RemoteAddr, err)
-		// We don't bail early here, we set badUser and let ACLs handle it all later. When you
+	user, err = proxy.authenticateClient(req)
+	if err != nil || user == authFailUser {
+		proxy.ctxLogf("client: %v: authentication error during connect: %v", req.RemoteAddr, err)
+		// We don't bail early here, we set authFailUser and let ACLs handle it all later. When you
 		// fail during CONNECT / tunnel establishment, clients usually don't handle it well. Clients
 		// will tend to handle errors better if we reject tunneled requests instead.
 	}
@@ -149,39 +222,42 @@ func (proxy *Inkfish) FilterConnect(w http.ResponseWriter, r *http.Request) (act
 	// Search for an MITM bypass directive
 	if proxy.bypassMitm(user, host) {
 		proxy.logConnect(ConnectLogEntry{
-			RemoteAddr:    r.RemoteAddr,
+			RemoteAddr:    req.RemoteAddr,
 			User:          user,
 			ConnectTarget: host,
 			Result:        "BYPASS",
 		})
+		proxy.Metrics.BypassConnects.Inc(1)
 		return ConnectBypass
+	} else {
+		proxy.logConnect(ConnectLogEntry{
+			RemoteAddr:    req.RemoteAddr,
+			User:          user,
+			ConnectTarget: host,
+			Result:        "MITM",
+		})
+		proxy.Metrics.MitmConnects.Inc(1)
+		return ConnectMitm
 	}
-	proxy.logConnect(ConnectLogEntry{
-		RemoteAddr:    r.RemoteAddr,
-		User:          user,
-		ConnectTarget: host,
-		Result:        "MITM",
-	})
-	return ConnectMitm
 }
 
-func (proxy *Inkfish) RequestFilter(connectReq *http.Request, scheme string, w http.ResponseWriter, req *http.Request) bool {
+func (proxy *Inkfish) filterRequest(connectReq *http.Request, scheme string, w http.ResponseWriter, req *http.Request) bool {
 	var user string
 	var err error
 	if scheme == "https" {
-		// Since this is an http request, we authenticate from the connect request
+		// Since this is an https request, basic auth should come from the CONNECT
 		user, err = proxy.authenticateClient(connectReq)
-		if err != nil || user == badUser {
+		if err != nil || user == authFailUser {
 			proxy.ctxLogf("client: %v: authentication error during request: %v", connectReq.RemoteAddr, err)
-			// Don't bail out, user will come back as INVALID and get rejected by ACL
+			// Don't bail out, user is set to INVALID and will be rejected by ACL
 		}
 	}
 	if scheme == "http" {
-		// This is a non-tunneled request or for whatever reason, we don't have cached creds.
+		// This is a non-tunneled request
 		user, err = proxy.authenticateClient(req)
-		if err != nil || user == badUser {
+		if err != nil || user == authFailUser {
 			proxy.ctxLogf("client: %v: authentication error during request: %v", req.RemoteAddr, err)
-			// Don't bail out, user will come back as INVALID and get rejected by ACL
+			// Don't bail out, user is set to INVALID and will be rejected by ACL
 		}
 	}
 
@@ -194,21 +270,221 @@ func (proxy *Inkfish) RequestFilter(connectReq *http.Request, scheme string, w h
 			RemoteAddr: req.RemoteAddr,
 			User:       user,
 			Method:     req.Method,
-			Url:        &aclUrl,
+			Url:        aclUrl,
 			Result:     "ALLOW",
 		})
+		proxy.Metrics.AcceptedRequests.Inc(1)
 		return true
+	} else {
+		proxy.logRequest(RequestLogEntry{
+			RemoteAddr: req.RemoteAddr,
+			User:       user,
+			Method:     req.Method,
+			Url:        aclUrl,
+			Result:     "DENY",
+			Reason:     "URL denied by policy",
+		})
+		proxy.Metrics.DeniedRequests.Inc(1)
+		errMsg := fmt.Sprintf("URL denied by policy: %s", sanitiseURL(aclUrl))
+		sendAccessDenied(w, errMsg)
+		return false
+	}
+}
+
+type ConnectAction int
+
+const (
+	ConnectMitm ConnectAction = 1 + iota
+	ConnectBypass
+	ConnectDeny
+)
+
+func (proxy *Inkfish) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// Entry point for client requests
+	if req.Method == "CONNECT" {
+		// Client requested a CONNECT tunnel from the proxy
+		filterAction := proxy.filterConnect(w, req)
+		if filterAction == ConnectMitm {
+			proxy.mitmConnect(w, req)
+		} else if filterAction == ConnectBypass {
+			proxy.bypassConnect(w, req)
+		} else if filterAction == ConnectDeny {
+			// The filter already sent the response
+		}
+	} else if req.URL.Scheme == "http" {
+		// Regular HTTP proxy requests, non-secure
+		rp := &httputil.ReverseProxy{
+			Director:      httpDirector,
+			FlushInterval: proxy.FlushInterval,
+			Transport: &http.Transport{
+				DisableCompression: true,
+			},
+		}
+		proxy.requestHandler(nil, "http", rp).ServeHTTP(w, req)
+	} else {
+		// Any non-http request should be a CONNECT!
+		errMsg := fmt.Sprintf("Unexpected scheme: %s", req.URL.Scheme)
+		sendAccessDenied(w, errMsg)
+	}
+}
+
+func (proxy *Inkfish) mitmConnect(w http.ResponseWriter, req *http.Request) {
+	hostname, _, err := net.SplitHostPort(req.Host)
+	if err != nil || hostname == "" {
+		log.Println("cannot determine cert name for " + req.Host)
+		http.Error(w, "no upstream", 503)
+		return
+	}
+	cert, err := proxy.CertSigner.signHost([]string{hostname})
+	if err != nil {
+		log.Println("cert", err)
+		http.Error(w, "no upstream", 503)
+		return
 	}
 
-	// Fall through, failed
-	proxy.logRequest(RequestLogEntry{
-		RemoteAddr: req.RemoteAddr,
-		User:       user,
-		Method:     req.Method,
-		Url:        &aclUrl,
-		Result:     "DENY",
-		Reason:     "denied by policy",
-	})
-	sendAccessDenied(w)
-	return false
+	sConfig := new(tls.Config)
+	if proxy.TLSServerConfig != nil {
+		*sConfig = *proxy.TLSServerConfig
+	}
+	sConfig.Certificates = []tls.Certificate{cert}
+
+	cconn, err := handshake(w, req, sConfig)
+	if err != nil {
+		log.Println("handshake", req.Host, err)
+		return
+	}
+	defer cconn.Close()
+
+	cConfig := new(tls.Config)
+	if proxy.TLSClientConfig != nil {
+		*cConfig = *proxy.TLSClientConfig
+
+	}
+	rp := &httputil.ReverseProxy{
+		Director: httpsDirector,
+		Transport: &http.Transport{
+			TLSClientConfig:    cConfig,
+			DisableCompression: true,
+		},
+		FlushInterval: proxy.FlushInterval,
+	}
+
+	ch := make(chan int)
+	wc := &onCloseConn{cconn, func() { ch <- 0 }}
+
+	err = http.Serve(&oneShotListener{wc}, proxy.requestHandler(req, "https", rp))
+	if err != nil {
+		// TODO: logging
+		// TODO: blip counter
+	}
+	<-ch
+}
+
+func (proxy *Inkfish) bypassConnect(w http.ResponseWriter, r *http.Request) {
+	destConn, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		return
+	}
+	clientConn, _, err := hijacker.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+	}
+	go transfer(destConn, clientConn)
+	go transfer(clientConn, destConn)
+}
+
+func transfer(destination io.WriteCloser, source io.ReadCloser) {
+	defer destination.Close()
+	defer source.Close()
+	io.Copy(destination, source)
+}
+
+func okHeader(r *http.Request) []byte {
+	return []byte(fmt.Sprintf("HTTP/%d.%d 200 OK\r\n\r\n", r.ProtoMajor, r.ProtoMinor))
+}
+
+// handshake hijacks w's underlying net.Conn, responds to the CONNECT request
+// and manually performs the TLS handshake. It returns the net.Conn or and
+// error if any.
+func handshake(w http.ResponseWriter, r *http.Request, config *tls.Config) (net.Conn, error) {
+	raw, _, err := w.(http.Hijacker).Hijack()
+	if err != nil {
+		http.Error(w, "no upstream", 503)
+		return nil, err
+	}
+	if _, err = raw.Write(okHeader(r)); err != nil {
+		raw.Close()
+		return nil, err
+	}
+	conn := tls.Server(raw, config)
+	err = conn.Handshake()
+	if err != nil {
+		conn.Close()
+		raw.Close()
+		return nil, err
+	}
+	return conn, nil
+}
+
+func unsetUserAgent(r *http.Request) {
+	if _, ok := r.Header["User-Agent"]; !ok {
+		// Explicitly disable User-Agent so it's not set to default value
+		r.Header.Set("User-Agent", "")
+	}
+}
+
+func httpDirector(r *http.Request) {
+	unsetUserAgent(r)
+	r.URL.Host = r.Host
+	r.URL.Scheme = "http"
+}
+
+func httpsDirector(r *http.Request) {
+	unsetUserAgent(r)
+	r.URL.Host = r.Host
+	r.URL.Scheme = "https"
+}
+
+// A oneShotListener implements net.Listener whos Accept only returns a
+// net.Conn as specified by c followed by an error for each subsequent Accept.
+type oneShotListener struct {
+	c net.Conn
+}
+
+func (l *oneShotListener) Accept() (net.Conn, error) {
+	if l.c == nil {
+		return nil, errors.New("closed")
+	}
+	c := l.c
+	l.c = nil
+	return c, nil
+}
+
+func (l *oneShotListener) Close() error {
+	return nil
+}
+
+func (l *oneShotListener) Addr() net.Addr {
+	return l.c.LocalAddr()
+}
+
+// A onCloseConn implements net.Conn and calls its f on Close.
+type onCloseConn struct {
+	net.Conn
+	f func()
+}
+
+func (c *onCloseConn) Close() error {
+	if c.f != nil {
+		c.f()
+		c.f = nil
+	}
+	return c.Conn.Close()
 }
