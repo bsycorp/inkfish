1a9dd152fdfa569824d3e45d50aae512eda9f6c1
Meng Xu <meng.xu@auspost.com.au>
2021-12-09 14:09:05 +1100

    Fix step of publishing new image

--- scripts/docker-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker login -u $DOCKERUSER -p $DOCKERPASS
-docker tag  bsycorp/inkfish:${TRAVIS_BRANCH} bsycorp/inkfish:latest
-docker tag  bsycorp/inkfish:${TRAVIS_BRANCH}-slim bsycorp/inkfish:latest-slim
-docker push bsycorp/inkfish
